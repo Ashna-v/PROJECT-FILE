fabaa9b8ff441c18349bd9c139424b7c66e3ce41
akhiash <ashnamaryjohn98@gmail.com>
2022-03-23 14:12:32 +0530

    delpoyed

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/bakersworld'));
+app.use(express.static('./dist/bakersworld'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/bakersworld/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/bakersworld/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
